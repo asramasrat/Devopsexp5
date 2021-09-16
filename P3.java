e01a45dffcb2b9b7a39be04d0086dabe5c91f60c
asramasrat <asra.masrat@somaiya.edu>
2021-09-16 16:50:38 +0530

    new commit

--- P3.java
+++ P3.java
@@ -1,17 +1,23 @@
-class P3
+
+
+public class P3
 {
-	public static void main(String[] args) {
-		int num = Integer.parseInt(args[0]);
-		if(num < 0)
-			System.out.println("Invalid Input");
-		else
-		{
-			double fact = 1;
-			for(int i=1; i<=num; i++)
-			{
-				fact *= i;
-			}
-			System.out.println("Factorial: "+fact);
-		}
-	}
+  public static void main(String args[]) 
+  {
+      int n = 5;
+      //Outer Loop for number of Rows
+      for(int i=0;i<n;i++)
+      {
+          // Inner loop for printing '*' in each column.
+          for(int j=0;j<n;j++)
+          {
+              // For first row and last row we print '*' and for every other row we print the '*' at boundary region. 
+              if(i==0 || i==n-1 || j==0 || j==n-1)
+              {
+              System.out.print("*  ");
+              }
+         }
+       }
+   }
 }
+        
